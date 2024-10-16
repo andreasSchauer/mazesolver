aff999a1593d9ae12f457150ea186775a44454a7
andreasSchauer <andreas.schauer@yahoo.com>
2024-10-16 19:00:39 +0200

    E1: added one more test

--- tests.py
+++ tests.py
@@ -29,9 +29,22 @@ class Tests(unittest.TestCase):
         )
 
     def test_maze_create_cells_rectangle(self):
-        num_cols = 10
+        num_cols = 12
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 30, 50)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+    def test_maze_create_cells_large(self):
+        num_cols = 120
+        num_rows = 100
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1._cells),
             num_cols,
