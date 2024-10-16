621d9fa479e06b4493e80beb2164544ab14172ad
andreasSchauer <andreas.schauer@yahoo.com>
2024-10-16 12:15:30 +0200

    B: added Point and Line class

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from window import Window
+from graphics import *
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(300, 300), Point(500,200))
+    win.draw_line(line, "#cbff2c")
     win.wait_for_close()
+
+
 
 main()
